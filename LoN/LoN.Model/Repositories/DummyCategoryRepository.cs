9c615819a7c83c8ec9f0eac582f60e545703c917
Erik--Vink <jhvink@student.avans.nl>
2015-05-11 15:55:27 +0200

    DummyCategory
    DummyCategory

--- LoN/LoN.Model/Repositories/DummyCategoryRepository.cs
+++ LoN/LoN.Model/Repositories/DummyCategoryRepository.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LoN.Model.Interfaces;
+using LoN.Model.Models;
 
 namespace LoN.Model.Repositories
 {
-    class DummyCategoryRepository
+    public class DummyCategoryRepository : IGenericRepository<Category>
     {
+
+        public Context.AppContext Context
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        IEnumerable<Category> IGenericRepository<Category>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Category IGenericRepository<Category>.GetOne(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int originalKey, Category updatedEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
