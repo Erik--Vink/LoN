cde2331cde2ce5b7a972635e350c56191dae594c
Erik--Vink <jhvink@student.avans.nl>
2015-05-11 16:57:37 +0200

    Implementation of dummy repo's
    Implementation of dummy repo's

--- LoN/LoN.Model/Repositories/DummyCategoryRepository.cs
+++ LoN/LoN.Model/Repositories/DummyCategoryRepository.cs
@@ -25,7 +25,17 @@ namespace LoN.Model.Repositories
 
         IEnumerable<Category> IGenericRepository<Category>.GetAll()
         {
-            throw new NotImplementedException();
+            List<Category> categories = new List<Category>
+            {
+                new Category(){ CategoryId = 0, CategoryName = "Head"},
+                new Category(){ CategoryId = 1, CategoryName = "Shoulders" },
+                new Category(){ CategoryId = 2, CategoryName = "Chest" },
+                new Category(){ CategoryId = 3, CategoryName = "Belt" },
+                new Category(){ CategoryId = 4, CategoryName = "Legs" },
+                new Category(){ CategoryId = 5, CategoryName = "Boots" }
+            };
+
+            return categories;
         }
 
         Category IGenericRepository<Category>.GetOne(int key)
