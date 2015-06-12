345636d2c5d1294f74bc00385b60b96a8146898d
Nevernown <ca.boel@student.avans.nl>
2015-06-12 12:50:52 +0200

    crud start

---
+++ LoN/LoN.View/ViewModel/CrudViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoN.View.ViewModel
+{
+    class CrudViewModel
+    {
+    }
+}
