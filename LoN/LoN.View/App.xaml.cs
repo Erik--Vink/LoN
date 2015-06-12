313c71c95ff6cddef65a2a6a82afb09dd5014aea
Erik--Vink <jhvink@student.avans.nl>
2015-06-12 12:44:48 +0200

    NinjaView added
    NinjaView added

--- LoN/LoN.View/App.xaml.cs
+++ LoN/LoN.View/App.xaml.cs
@@ -13,5 +13,29 @@ namespace LoN.View
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            ToShopView();
+        }
+
+        public void ToShopView()
+        {
+            var x = new NinjaWindow() { Top = 100, Left = 100 };
+            x.Show();
+            new MainWindow() { Top = x.Top, Left = (x.Left + x.Width) }.Show();
+            var crud = Current.Windows.OfType<Window>().SingleOrDefault(w => w.Name == "Crud");
+            if (crud != null) { crud.Close(); }
+        }
+
+        //public void ToCrudView()
+        //{
+        //    new CrudWindow() { Top = 100, Left = 100 }.Show();
+        //    var main = Current.Windows.OfType<Window>().SingleOrDefault(w => w.Name == "Main");
+        //    if (main != null) { main.Close(); }
+        //    var list = Current.Windows.OfType<Window>().SingleOrDefault(w => w.Name == "List");
+        //    if (list != null) { list.Close(); }
+        //}
+
+
     }
 }
